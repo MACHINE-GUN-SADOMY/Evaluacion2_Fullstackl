6c7e9fefd6e21bf078ca4644485b82f79302a832
Chainsaw Sadomator <109769365+MACHINE-GUN-SADOMY@users.noreply.github.com>
2025-05-22 23:47:13 -0400

    Se agrega Estructura de Spring junto a sus dependencias y Maven.

---
+++ src/main/java/cl/duoc/evaluacion/cparra/hohidalgo/Evaluacio_2_Fullstackl/Evaluacio2FullstacklApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.evaluacion.cparra.hohidalgo.Evaluacio_2_Fullstackl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacio2FullstacklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacio2FullstacklApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/evaluacion/cparra/hohidalgo/Evaluacio_2_Fullstackl/Evaluacio2FullstacklApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.evaluacion.cparra.hohidalgo.Evaluacio_2_Fullstackl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacio2FullstacklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
